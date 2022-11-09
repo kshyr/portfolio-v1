25df1f6b0ffb959bf1d2d53238032e9ed442458a
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-08 23:12:46 -0500

    add text swap animation and gradient on name
    description header and hidden scrollbar

--- components/Welcome.js
+++ components/Welcome.js
@@ -9,10 +9,21 @@ export default function Welcome() {
         <br></br> <span className="text-reg">My name is</span>
         <br></br>
         <div className="inline-block">
-          <span className="text-red text-name font-semibold hover:rotate-12">
-            Kostiantyn
+          <span className=" bg-gradient-to-r from-blue to-[#2b5665] text-transparent bg-clip-text text-name font-semibold">
+            Kostiantyn&nbsp;
           </span>
-          <span className="text-blue text-name font-semibold"> Shyrolapov</span>
+          <span className="bg-gradient-to-r from-[#b29d36] to-red text-transparent bg-clip-text text-name font-semibold hover:rotate-12">
+            Shyrolapov
+          </span>
+        </div>
+        <div className="w-48 border-t-2 border-spacing-x-56 border-black mt-10 opacity-75"></div>
+        <div className="mt-10 mb-5 ml-32 mr-32 text-center">
+          <h3 class="relative text-3xl opacity-75 inline">
+            frontend engineer that builds web applications with
+          </h3>
+          <div className="bg-black text-3xl rounded-md h-10 w-40 ml-4 translate-y-[-20%] inline-flex justify-center items-center overflow-hidden">
+            <div className="font-bold text-blue before:animate-textswap relative before:absolute flex justify-center items-center"></div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   theme: {
     colors: {
+      transparent: "transparent",
       black: "#242331",
       wheat: "#e0cfa3",
       "l-wheat": "#f5efe0",
@@ -15,12 +16,115 @@ module.exports = {
     extend: {
       animation: {
         wiggle: "wiggle 6s ease-in-out infinite",
+        textswap: "textswap 12s ease-in-out infinite",
       },
       keyframes: {
         wiggle: {
           "0%, 80%": { transform: "rotate(0)" },
           "85%, 95%": { transform: "rotate(-14deg)" },
           "90%": { transform: "rotate(14deg)" },
+        },
+        textswap: {
+          "0%": {
+            content: '"React"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "15%": {
+            content: '"React"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "20%": {
+            content: '"React"',
+            marginTop: "70px",
+            opacity: "0",
+          },
+          "25%": {
+            content: '""',
+            marginTop: "-100px",
+            opacity: "0",
+          },
+          "30%": {
+            content: '"Next.js"',
+            marginTop: "6px",
+            opacity: "1",
+          },
+          "32%": {
+            content: '"Next.js"',
+            marginTop: "-2px",
+            opacity: "1",
+          },
+          "33%": {
+            content: '"Next.js"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "45%": {
+            content: '"Next.js"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "50%": {
+            content: '"Next.js"',
+            marginTop: "70px",
+            opacity: "0",
+          },
+          "55%": {
+            content: '""',
+            marginTop: "-100px",
+            opacity: "0",
+          },
+          "60%": {
+            content: '"GraphQL"',
+            marginTop: "6px",
+            opacity: "1",
+          },
+          "62%": {
+            content: '"GraphQL"',
+            marginTop: "-2px",
+            opacity: "1",
+          },
+          "63%": {
+            content: '"GraphQL"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "75%": {
+            content: '"GraphQL"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "80%": {
+            content: '"GraphQL"',
+            marginTop: "70px",
+            opacity: "0",
+          },
+          "85%": {
+            content: '""',
+            marginTop: "-100px",
+            opacity: "0",
+          },
+          "90%": {
+            content: '"React"',
+            marginTop: "6px",
+            opacity: "1",
+          },
+          "92%": {
+            content: '"React"',
+            marginTop: "-2px",
+            opacity: "1",
+          },
+          "93%": {
+            content: '"React"',
+            marginTop: "0",
+            opacity: "1",
+          },
+          "100%": {
+            content: '"React"',
+            marginTop: "0",
+            opacity: "1",
+          },
         },
       },
     },
